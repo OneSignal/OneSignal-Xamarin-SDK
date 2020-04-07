af4787069dd18cda4c323fe161ed097ae2572ca6
mikechoch <mdicioccio25@gmail.com>
2020-04-06 18:58:58 -0700

    Properly null checking all push and email success status * Printing everything out so its easy to understand response payload structure

--- Samples/Com.OneSignal.Sample.Shared/SharedPush.cs
+++ Samples/Com.OneSignal.Sample.Shared/SharedPush.cs
@@ -46,9 +46,33 @@ namespace Com.OneSignal.Sample.Shared
          OneSignalOutcomeEventDemo();
       }
 
-      private static void OneSignalSetExternalUSerId(Dictionary<string , object> results)
+      private static void OneSignalSetExternalUSerId(Dictionary<string, object> results)
       {
+         // The results will contain push and email success statuses
          Debug.WriteLine("External user id updated with results: " + Json.Serialize(results));
+
+         // Push can be expected in almost every situation with a success status, but
+	      // as a pre-caution its good to verify it exists
+         if (results.ContainsKey("push"))
+         {
+            Dictionary<string, object> pushStatusDict = results["push"] as Dictionary<string, object>;
+            if (pushStatusDict.ContainsKey("success"))
+            {
+               Debug.WriteLine("External user id updated for push with results: " + pushStatusDict["success"] as string);
+            }
+            
+         }
+
+         // Verify the email is set or check that the results have an email success status
+         if (results.ContainsKey("email"))
+         {
+            Dictionary<string, object> emailStatusDict = results["email"] as Dictionary<string, object>;
+            if (emailStatusDict.ContainsKey("success"))
+            {
+               Debug.WriteLine("External user id updated for email with results: " + emailStatusDict["success"] as string);
+            }
+            
+         }
       }
 
       private static void OneSignalInAppMessagingDemo()
