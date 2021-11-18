29a3ac830e04bf928ef61ad5d515699c48efce18
Tanay Nigam <tanaynigam12@gmail.com>
2021-11-18 13:11:01 -0800

    Add delegate function declarations
    * Add delegate function declarations

---
+++ Com.OneSignal.Core/Delegate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+//using Org.Json;
+
+namespace Com.OneSignal.Core {
+
+   public delegate void InAppMessageClicked(InAppMessageAction action);
+
+   public delegate void OnSetSMSSuccess(Dictionary<string, object> results);
+   public delegate void OnSetSMSFailure(Dictionary<string, object> response);
+
+   public delegate void OnSetEmailSuccess();
+   public delegate void OnSetEmailFailure(Dictionary<string, object> results);
+
+   public delegate void OnExternalUserIdUpdateSuccess(Dictionary<string, object> results);
+   public delegate void OnExternalUserIdUpdateFailure(Dictionary<string, object> response);
+
+   //public delegate void OnTagsReceivedSuccess(JSONObject results);
+   public delegate void OnTagsReceivedFailure(Dictionary<string, object> response);
+
+   public delegate void TagsReceived(Dictionary<string, object> tags);
+
+   public delegate void OnPostNotificationSuccess(Dictionary<string, object> results);
+   public delegate void OnPostNotificationFailure(Dictionary<string, object> response);
+
+   public delegate void SendOutcomeEventSuccess(OutcomeEvent outcomeEvent);
+}
