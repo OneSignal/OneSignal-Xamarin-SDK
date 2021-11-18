080598d9dc93675a9bf24317eba3635be049faf2
Tanay Nigam <tanaynigam12@gmail.com>
2021-11-18 13:02:52 -0800

    Add shared enums to OneSignal.Core
    * Add shared enums LogType and LogoutOptions to OneSignal.Core

---
+++ Com.OneSignal.Core/LogType.cs
@@ -0,0 +1,5 @@
+﻿using System;
+
+namespace Com.OneSignal.Core {
+    public enum LogType { NONE, FATAL, ERROR, WARN, INFO, DEBUG, VERBOSE }
+}
---
+++ Com.OneSignal.Core/LogoutOptions.cs
@@ -0,0 +1,5 @@
+﻿using System;
+
+namespace Com.OneSignal.Core {
+    public enum LogoutOptions { Email, SMS, ExternalUserId }
+}
