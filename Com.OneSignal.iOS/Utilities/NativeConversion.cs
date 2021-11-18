568024ab1f0432e929fd647921e8726f289e1b12
Tanay Nigam <tanaynigam12@gmail.com>
2021-11-18 13:38:56 -0800

    Add iOS helper class for native conversion
    * Add helper class in iOS for conversion of iOS native states to Xamarin states

---
+++ Com.OneSignal.iOS/Utilities/NativeConversion.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Foundation;
+using Com.OneSignal.Core;
+
+namespace Com.OneSignal {
+    public static class NativeConversion {
+
+        public static Dictionary<string, object> NSDictToPureDict(NSDictionary nsDict) {
+            if (nsDict == null)
+                return null;
+            NSError error;
+            NSData jsonData = NSJsonSerialization.Serialize(nsDict, 0, out error);
+            NSString jsonNSString = NSString.FromData(jsonData, NSStringEncoding.UTF8);
+            string jsonString = jsonNSString.ToString();
+            return Json.Deserialize(jsonString) as Dictionary<string, object>;
+        }
+
+        public static string NSDictToString(NSDictionary nsDict) {
+            if (nsDict == null)
+                return null;
+            NSError error;
+            NSData jsonData = NSJsonSerialization.Serialize(nsDict, 0, out error);
+            NSString jsonNSString = NSString.FromData(jsonData, NSStringEncoding.UTF8);
+            return jsonNSString.ToString();
+        }
+
+        public static NSDictionary DictToNSDict(Dictionary<string, object> dict) {
+            if (dict == null)
+                return null;
+
+            string jsonString = Json.Serialize(dict);
+            NSString jsonNSString = new NSString(jsonString);
+            NSData jsonData = jsonNSString.Encode(NSStringEncoding.UTF8);
+            NSError error;
+            NSDictionary nsDict = NSJsonSerialization.Deserialize(jsonData, 0, out error) as NSDictionary;
+
+            return nsDict;
+        }
+    }
+}
