6ea9048c4eca97289d6df0d6028d6e18b786b2d6
Tanay Nigam <tanaynigam12@gmail.com>
2021-11-18 12:47:27 -0800

    Delete Com.OneSignal.Abstractions
    * Delete project Com.OneSignal.Abstractions
    * Delete all files contained in Com.OneSignal.Abstractions

--- Com.OneSignal.Abstractions/IOSSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Com.OneSignal.Abstractions
-{
-   public class IOSSettings
-   {
-      public const string kOSSettingsKeyAutoPrompt = "kOSSettingsKeyAutoPrompt";
-      public const string kOSSettingsKeyInAppLaunchURL = "kOSSettingsKeyInAppLaunchURL";
-   }
-}
--- Com.OneSignal.Abstractions/LOG_LEVEL.cs
+++
@@ -1,5 +0,0 @@
-﻿using System;
-namespace Com.OneSignal.Abstractions
-{
-    public enum LOG_LEVEL { NONE, FATAL, ERROR, WARN, INFO, DEBUG, VERBOSE }
-}
