a5794adbdce96484eb5ce44044b9333683fa69a3
Tanay Nigam <tanaynigam12@gmail.com>
2021-11-22 13:29:04 -0800

    Update Laters class

--- Com.OneSignal.Core/Utilities/Later.cs
+++ Com.OneSignal.Core/Utilities/Later.cs
@@ -3,59 +3,62 @@ using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace Laters {
-    /// <summary>Read-only interface for a standard <see cref="Later{TResult}"/></summary>
-    public interface ILater<TResult> {
-        event Action<TResult> OnComplete;
-        TaskAwaiter<TResult> GetAwaiter();
-    }
+   /// <summary>For referencing <see cref="ILater{TResult}"/> generically</summary>
+   public interface ILater { }
 
-    /// <summary>Basic Later for passing a single type to callbacks and awaiters</summary>
-    public class Later<TResult> : BaseLater<TResult> {
-        public void Complete(TResult result) => _complete(result);
-    }
+   /// <summary>Read-only interface for a standard <see cref="Later{TResult}"/></summary>
+   public interface ILater<TResult> : ILater {
+      event Action<TResult> OnComplete;
+      TaskAwaiter<TResult> GetAwaiter();
+   }
 
-    /// <summary>Separated implementation so the derivations can offer different methods for completion</summary>
-    public abstract class BaseLater<TResult> : ILater<TResult> {
-        public event Action<TResult> OnComplete {
-            remove => _onComplete -= value;
-            add {
-                if (_isComplete)
-                    _onComplete += value;
-                else if (value != null)
-                    value(_result);
-            }
-        }
+   /// <summary>Basic Later for passing a single type to callbacks and awaiters</summary>
+   public class Later<TResult> : BaseLater<TResult> {
+      public void Complete(TResult result) => _complete(result);
+   }
 
-        public TaskAwaiter<TResult> GetAwaiter() {
-            if (_completionSource != null)
-                return _completionSource.Task.GetAwaiter();
-
-            _completionSource = new TaskCompletionSource<TResult>();
-
+   /// <summary>Separated implementation so the derivations can offer different methods for completion</summary>
+   public abstract class BaseLater<TResult> : ILater<TResult> {
+      public event Action<TResult> OnComplete {
+         remove => _onComplete -= value;
+         add {
             if (_isComplete)
-                _completionSource.TrySetResult(_result);
-            else
-                _onComplete += result => _completionSource.TrySetResult(result);
+               _onComplete += value;
+            else if (value != null)
+               value(_result);
+         }
+      }
 
+      public TaskAwaiter<TResult> GetAwaiter() {
+         if (_completionSource != null)
             return _completionSource.Task.GetAwaiter();
-        }
 
-        protected void _complete(TResult result) {
-            if (_isComplete)
-                return;
+         _completionSource = new TaskCompletionSource<TResult>();
 
-            _result = result;
+         if (_isComplete)
+            _completionSource.TrySetResult(_result);
+         else
+            _onComplete += result => _completionSource.TrySetResult(result);
 
-            var toComplete = _onComplete;
-            _onComplete = null;
+         return _completionSource.Task.GetAwaiter();
+      }
 
-            _isComplete = true;
-            toComplete?.Invoke(_result);
-        }
+      protected void _complete(TResult result) {
+         if (_isComplete)
+            return;
 
-        private TaskCompletionSource<TResult> _completionSource;
-        private event Action<TResult> _onComplete;
-        private bool _isComplete = false;
-        private TResult _result;
-    }
+         _result = result;
+
+         var toComplete = _onComplete;
+         _onComplete = null;
+
+         _isComplete = true;
+         toComplete?.Invoke(_result);
+      }
+
+      private TaskCompletionSource<TResult> _completionSource;
+      private event Action<TResult> _onComplete;
+      private bool _isComplete = false;
+      private TResult _result;
+   }
 }
